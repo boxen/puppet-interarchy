701259a2bcf9684e6868562e70d73a805a73230e
Gabe Blair <gabe@castironcoding.com>
2013-03-09 16:06:29 -0800

    Fix wrong download URL

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,8 @@
-class interarchy {
+class interarchy (
+	$version = '1004',
+  ) {
   package { 'Interarchy':
     provider => 'compressed_app',
-    source   => "http://nolobe.com/interarchy/download",
+    source   => "http://downloads.nolobe.com/interarchy${version}.zip",
   }
 }
